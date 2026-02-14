bae2b6a27d1fca4ef046760311057dc85fa3179b
vijay-iptv <vijayan.mca31@gmail.com>
2026-02-14 19:55:46 +0530

    first commitddddd

---
+++ api/proxy.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).send("Missing URL");
+  }
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+        "Referer": "https://www.jiotv.com/"
+      }
+    });
+
+    const buffer = await response.arrayBuffer();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Content-Type", response.headers.get("content-type"));
+    res.status(200).send(Buffer.from(buffer));
+
+  } catch (err) {
+    res.status(500).send("Proxy failed");
+  }
+}
